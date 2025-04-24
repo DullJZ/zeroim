36f50fe8bd9ad75432a21d36c3849da21c9ba52c
DullJZ <79080562+DullJZ@users.noreply.github.com>
2025-04-24 17:51:53 +0800

    响应输出

--- apps/user/api/user.go
+++ apps/user/api/user.go
@@ -7,9 +7,11 @@ import (
 	"github.com/DullJZ/zeroim/apps/user/api/internal/config"
 	"github.com/DullJZ/zeroim/apps/user/api/internal/handler"
 	"github.com/DullJZ/zeroim/apps/user/api/internal/svc"
+	"github.com/DullJZ/zeroim/pkg/resultx"
 
 	"github.com/zeromicro/go-zero/core/conf"
 	"github.com/zeromicro/go-zero/rest"
+	"github.com/zeromicro/go-zero/rest/httpx"
 )
 
 var configFile = flag.String("f", "etc/user.yaml", "the config file")
@@ -25,6 +27,9 @@ func main() {
 
 	ctx := svc.NewServiceContext(c)
 	handler.RegisterHandlers(server, ctx)
+
+	httpx.SetErrorHandlerCtx(resultx.ErrHandler(c.Name))
+	httpx.SetOkHandler(resultx.OkHandler)
 
 	fmt.Printf("Starting server at %s:%d...\n", c.Host, c.Port)
 	server.Start()
